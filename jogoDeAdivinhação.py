13ec0c3daba4cb5656b9743eda1994e0f9c3b404
LEONARDO SOUZA LEANDRO <128157527+LEONARDO-LEANDRO@users.noreply.github.com>
2024-03-13 09:44:44 -0300

    Add files via upload

---
+++ jogoDeAdivinhação.py
@@ -0,0 +1,31 @@
+print('*********************************')
+print('Bem vindo, ao JOGO DE ADIVINHAÇÃO')
+print('*********************************')
+
+#Definindo o número secreto
+numeroSecreto = 38
+
+#Definindo o número de tentativas
+numeroTentativas = 3
+rodada = 1
+
+while(rodada <= numeroTentativas):
+    print('Você tem somente algumas chances -->' ,numeroTentativas,', Não desperdice, verme insolente!!')
+    print('Tentativa', rodada, 'de' , numeroTentativas)
+    
+#Recebendo o chute do jogador
+    chuteString = input('Digite um número: ')
+    chute = int(chuteString)
+
+#Declarando as condições
+    if (numeroSecreto == chute):
+        print('Você acertou!')
+        break
+    elif(chute>numeroSecreto):
+        print('Você errou!! O número secreto é um número menor')
+    else:
+        print('Você errou!! O número secreto é um número maior')
+
+    #numeroTentativas = numeroTentativas-1
+    rodada = rodada +1
+#Aula Elif 26.02.24
