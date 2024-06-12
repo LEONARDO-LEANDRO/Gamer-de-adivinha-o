aa831aefcfc3d24dae8e6e3beeaca4ad0c4a8865
LEONARDO SOUZA LEANDRO <128157527+LEONARDO-LEANDRO@users.noreply.github.com>
2024-06-12 08:57:43 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -3,6 +3,7 @@ def jogar_forca():
     print('*********************************')
     print('Bem vindo, ao JOGO FORCA')
     print('*********************************')
+    
     #Definir qual a palavra secreta
     palavraSecreta = "feijao"
 
@@ -12,9 +13,10 @@ def jogar_forca():
     #Enquanto o jogador não se "enforcar" E não acertar a palavra, faça algo
     while(not enforcou and not acertou):
         chute = input("Qual a letra?")
+        chute = chute.strip()
 
         index = 0
         for letra in palavraSecreta:
-            if(chute == letra):
+            if(chute.upper() == letra):
                 print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index + 1
